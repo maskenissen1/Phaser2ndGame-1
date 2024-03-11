d33693a6e0a93841c8552cde6313cec01e4df046
maskenissen1 <159002509+maskenissen1@users.noreply.github.com>
2024-03-11 11:21:30 +0200

    Я БАГАТО ЧОГО ЗРРОБИВ ДААААВ
    впіхащ

--- game.js
+++ game.js
@@ -19,68 +19,28 @@ var config = { //Налаштовуємо сцену
 };
 
 var game = new Phaser.Game(config);
-
-//змінні рлрпда
+var player;
+var platform;
 var score = 0;
 var scoreText;
+var worldWidth = config.worldWidth * 2;
+var star;
+var alien;
+var spaceship;
 var kiwi;
-var bomb;
+
 var record = 0
-
-//Функція підбору зірок
-function collectKiwi (player, kiwi)
-{
-    kiwi.disableBody(true, true);
-
-    //Нараховуємо бали
-    score += 10;
-    scoreText.setText('Score: ' + score);
-
-    //Створення бромб
-    var bomb = bombs.create(x, 16, 'bomb').setScale(1);
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-
-
-    //Перестворення зірочок
-    if (kiwis.countActive(true) === 0)
-    {
-        kiwis.children.iterate(function (child) {
-
-            child.enableBody(true, child.x, 0, true, true);
-            child.anims.play('kiwi_anim');
-            child.setScale(1.5);
-        });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-    }
-}
-
-//Після зіткнення 3
-function hitBomb (player, bomb)
-{
-    this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play('turn');
-    gameOver = true;
-
-    this.scene.restart();
-
-    if(score > record){
-        record = score;
-        document.getElementById("record_text").innerHTML = "Рекорд :<br/>" + record;
-    }
-    score = 0
-}
 
 function preload () //Завантажуємо графіку для гри
 {
-    this.load.image('cheese', 'assets/cheese.png');
-    this.load.image('ground', 'assets/moon.png');
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('ground', 'assets/platform.png');
     this.load.image('kiwi', 'assets/kiwi.png');
+    this.load.image('spaceship', 'assets/spaceship.png');
+    this.load.image('alien', 'assets/alien.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('fon+', 'assets/fon+.png');
+    this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 32 }
@@ -91,51 +51,82 @@ function preload () //Завантажуємо графіку для гри
     );
 }
 
-
 function create ()
 {
-    
+
     //Додаемо небо
-    this.add.image(960, 540, 'sky').setScale(3);
+
+    this.add.tileSprite(0, 0, worldWidth, 1080, "fon+")
+        .setOrigin(0, 0)
+        .setScale(2)
+        .setDepth(0);
 
     //Створюемо текст з рахунком
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    //scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 
     //Ініціалізуємо курсор Phaser
     cursors = this.input.keyboard.createCursorKeys();
 
-    //Створюемо фізичну групу платформ
+    //Створюемо фізичну групу
     platforms = this.physics.add.staticGroup();
+    kiwi = this.physics.add.staticGroup();
+    star = this.physics.add.group();
+    alien = this.physics.add.group();
+    spaceship = this.physics.add.group();
 
     //Створюемо платформи
-    platforms.create(955, 880, 'ground').refreshBody().setScale(1);
+    for (var x = 0; x < worldWidth; x = x + 400){
+        console.log(x);
+        platforms.create(x, 1080, 'ground').setOrigin(0, 0).refreshBody().setDepth(1);
+    }
 
-    //створюємо ківіси
-    
+    for (let i = 0; i < 5; i++) {
+        platforms.create(960 + i * 1920, 900, 'ground').refreshBody().setScale(1).setDepth(1);
+    }
+    //Створюємо об'єкти декорації
+    for (let x = 0; x < worldWidth; x += Phaser.Math.FloatBetween(400, 750)) {
+        star.create(x, 180, 'star')
+            .setOrigin(0.5, 0.5)
+            .setScale(Phaser.Math.FloatBetween(0.05, 0.1))
+            .setDepth(Phaser.Math.Between(1,10));
+    }
 
+    for (let x = 0; x < worldWidth; x += Phaser.Math.FloatBetween(1500, 2000)) {
+        alien.create(x, 180, 'alien')
+            .setOrigin(0.5, 0.5)
+            .setScale(Phaser.Math.FloatBetween(0.3, 0.3))
+            .setDepth(Phaser.Math.Between(1,10));
+    }
 
-    // Створюємо та налаштовуємо групу зірок
-    stars = this.physics.add.group({
-        key: 'kiwi', // Змінено ключ на 'cheese'
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-
-
+    for (let x = 0; x < worldWidth; x += Phaser.Math.FloatBetween(1000, 1250)) {
+        spaceship.create(x, 180, 'spaceship')
+            .setOrigin(0.5, 0.5)
+            .setScale(Phaser.Math.FloatBetween(0.5, 1))
+            .setDepth(Phaser.Math.Between(1,10));
+    }
 
     //Створюємо та налаштовуємо спрайт гравця
-    player = this.physics.add.sprite(100, 0, 'dude').setScale(2);
+    player = this.physics.add.sprite(960, 1, 'dude').setScale(2).setDepth(4);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
+    //Налаштування камери
+    this.cameras.main.setBounds(0, 0, worldWidth, window.innerHeight);
+    this.physics.world.setBounds(0, 0, worldWidth, window.innerHeight);
+
+    //Слідкування камери за гравцем
+    this.cameras.main.startFollow(player)
+
+    //ківіси
+    
 
     //Створюемо та налаштовуємо фізичний об'єкт бомби
     bombs = this.physics.add.group();
     this.physics.add.collider(bombs, platforms);
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
+    this.physics.add.collider(player, bombs, null, this);
 
     //Змінено гравітацію гравця
     player.body.setGravityY(0)
-
 
     //Створюємо та налаштовуємо анімації
     this.anims.create({
@@ -147,7 +138,7 @@ function create ()
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
+        frames: [{ key: 'dude', frame: 4 }],
         frameRate: 20
     });
 
@@ -158,12 +149,14 @@ function create ()
         repeat: -1
     });
 
-    //Додано колізії між гравцем та платформами
+    //Додано колізії
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(star, platforms);
+    this.physics.add.collider(alien, platforms);
+    this.physics.add.collider(spaceship, platforms);
 
-    this.cameras.main.setBounds(0, 0, 5000, 1080); // Встановлюємо межі камери на всю ширину та висоту сцени
-this.cameras.main.startFollow(player);
 }
+
 
 function update ()
 {
